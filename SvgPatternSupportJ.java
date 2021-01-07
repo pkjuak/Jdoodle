34b1cad9ccfe076bbc77738f6c6918aec9f4d91c
Peter Makoi <31699860+pkjuak@users.noreply.github.com>
2021-01-07 16:44:06 +0100

    Update SvgPatternSupportJ.java

--- SvgPatternSupportJ.java
+++ SvgPatternSupportJ.java
@@ -1,4 +1,5 @@
 public void convert(String svg, String output, PageSize size) throws IOException {
+   
    try (PdfDocument doc = new PdfDocument(new PdfWriter(output, new WriterProperties().setCompressionLevel(0)))) {
        doc.addNewPage(size);
        ISvgConverterProperties properties = new SvgConverterProperties().setBaseUri(svg);

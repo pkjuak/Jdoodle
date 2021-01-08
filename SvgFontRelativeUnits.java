4132c2f7d785e8c6b093b757ffcb4c1cd0821060
Peter Makoi <31699860+pkjuak@users.noreply.github.com>
2021-01-08 10:58:56 +0100

    Create SvgFontRelativeUnits.java

---
+++ SvgFontRelativeUnits.java
@@ -0,0 +1,7 @@
+public void convert(String svg, String output, PageSize size) throws IOException {
+   try (PdfDocument doc = new PdfDocument(new PdfWriter(output, new WriterProperties().setCompressionLevel(0)))) {
+       doc.addNewPage(size);
+       ISvgConverterProperties properties = new SvgConverterProperties().setBaseUri(svg);
+       SvgConverter.drawOnDocument(new FileInputStream(svg), doc, 1, properties);
+   }
+}

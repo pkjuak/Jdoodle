b3965c376a8455ea4a3bd6cdaeea2f06c971d295
Peter Makoi <31699860+pkjuak@users.noreply.github.com>
2021-07-01 16:56:13 +0200

    Update Type3fonts.java

--- Type3fonts.java
+++ Type3fonts.java
@@ -1,20 +1,44 @@
-public void manipulatePdf(String dest) throws IOException {
-   //Load the license file to use cleanup features
-   LicenseKey.loadLicenseFile("license.xml");
-   PdfDocument pdfDocument = new PdfDocument(new PdfReader(SRC), new PdfWriter(dest));
-   PdfAcroForm pdfAcroForm = PdfAcroForm.getAcroForm(pdfDocument, false);
+import com.itextpdf.forms.PdfAcroForm;
+import com.itextpdf.kernel.colors.ColorConstants;
+import com.itextpdf.kernel.colors.DeviceRgb;
+import com.itextpdf.kernel.geom.Rectangle;
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfReader;
+import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.kernel.pdf.StampingProperties;
+import com.itextpdf.licensekey.LicenseKey;
+import com.itextpdf.pdfcleanup.PdfCleanUpLocation;
+import com.itextpdf.pdfcleanup.PdfCleanUpTool;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
-   if (pdfAcroForm != null) {
-       pdfAcroForm.flattenFields();
-   }
-   DeviceRgb color = (DeviceRgb) ColorConstants.BLACK;
+public class CleanupTest {
+    
+    public static void main(String[] args) throws IOException {
+       
+        //Load the license file to use cleanup features
+        LicenseKey.loadLicenseFile("license.xml");
+        
+        StampingProperties properties = new StampingProperties();
+        properties.useAppendMode();
 
-   List<PdfCleanUpLocation> cleanUpLocations = new ArrayList<PdfCleanUpLocation>();
-   cleanUpLocations.add(new PdfCleanUpLocation(1, new Rectangle(50, 50, 500, 800), color));
-   PdfCleanUpTool cleaner = new PdfCleanUpTool(pdfDocument, cleanUpLocations);
+        PdfDocument pdfDocument = new PdfDocument(new PdfReader("base.pdf"), new PdfWriter("cleanupResult.pdf"), properties);
+        PdfAcroForm pdfAcroForm = PdfAcroForm.getAcroForm(pdfDocument, false);
 
-   cleaner.cleanUp();
+        if (pdfAcroForm != null) {
+            pdfAcroForm.flattenFields();
+        }
+       
+        DeviceRgb color = (DeviceRgb) ColorConstants.BLACK;
 
-   pdfDocument.close();
+        List<PdfCleanUpLocation> cleanUpLocations = new ArrayList<PdfCleanUpLocation>();
+        cleanUpLocations.add(new PdfCleanUpLocation(1, new Rectangle(10, 300, 600, 400), color));
+        PdfCleanUpTool cleaner = new PdfCleanUpTool(pdfDocument, cleanUpLocations);
+
+        cleaner.cleanUp();
+
+        pdfDocument.close();
+    }
 }
 
